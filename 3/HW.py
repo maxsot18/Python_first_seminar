a4b5d8b808987212d18fbcf58a69409470900fe7
maxsot <sotnikov.maxim3007@gmail.com>
2023-02-06 21:12:36 +0600

    ALL HW

--- 3/HW.py
+++ 3/HW.py
@@ -7,20 +7,83 @@
 # 1 2 3 4 5
 # 3
 # -> 1
-import random
-array = []
-n = int(input("Enter a length of array:"))
-count = 0
-x = random.randint(0, 10)
-print(f"The number to find is : {x}")
-for i in range(n):
-    value = random.randint(0, 10)
-    array.append(value)
-print(array)  
+# import random
+# array = []
+# n = int(input("Enter a length of array:"))
+# count = 0
+# x = random.randint(0, 10)
+# print(f"The number to find is : {x}")
+# for i in range(n):
+#     value = random.randint(0, 10)
+#     array.append(value)
+# print(array)  
 
-for i in range(len(array)):
-    if array[i] == x:
-        count += 1
+# for i in range(len(array)):
+#     if array[i] == x:
+#         count += 1
            
-print(count)         
+# print(count)         
+
+# Задача 2.
+# Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X. 
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+# В последующих строках записаны N целых чисел Ai. Последняя строка содержит число X.
+# Пример
+# 5
+# 1 2 3 4 5
+# 6
+# -> 5
+
+# array = []
+# n = int(input("enter a quantity of elements in array: "))
+# x = int(input("Enter a number: "))
+
+# for i in range(n):
+#     i += 1
+#     element = i
+#     array.append(element)
+# print(array)
+
+# if n <= x:
+#     print(array[-1])
+# else:
+#     print(array[x - 1])
+
+# Задача 20: В настольной игре Скрабл (Scrabble) каждая буква имеет определенную
+# ценность. В случае с английским алфавитом очки распределяются так:
+# ● A, E, I, O, U, L, N, S, T, R – 1 очко;
+# ● D, G – 2 очка;
+# ● B, C, M, P – 3 очка;
+# ● F, H, V, W, Y – 4 очка;
+# ● K – 5 очков;
+# ● J, X – 8 очков;
+# ● Q, Z – 10 очков.
+# А русские буквы оцениваются так:
+# ● А, В, Е, И, Н, О, Р, С, Т – 1 очко;
+# ● Д, К, Л, М, П, У – 2 очка;
+# ● Б, Г, Ё, Ь, Я – 3 очка;
+# ● Й, Ы – 4 очка;
+# ● Ж, З, Х, Ц, Ч – 5 очков;
+# ● Ш, Э, Ю – 8 очков;
+# ● Ф, Щ, Ъ – 10 очков.
+# Напишите программу, которая вычисляет стоимость введенного пользователем слова.
+# Будем считать, что на вход подается только одно слово, которое содержит либо только
+# английские, либо только русские буквы.
+
+ar1 = {"a": 1,"e": 1,"i": 1,"u": 1,"l": 1,"n": 1,"s": 1,"t": 1,"r": 1,"d": 2, "g": 2, "b": 3, "c": 3, "m": 3, "p": 3,"f": 4, "h": 4, "v": 4, "w": 4, "y": 4,"k": 5,"j": 8, "x": 8,"q": 10, "z": 10,"а": 1, "в": 1, "и": 1, "н": 1, "о": 1,"н": 1, "р": 1, "с": 1, "т": 1, "д": 2, "к": 2, "л": 2, "м": 2, "п": 2, "у": 2, "б": 3, "г": 3, "ь": 3, "я": 3, "й": 4, "ы": 4, "ж": 5, "з": 5, "х": 5, "ц": 5, "ч": 5, "ш": 8, "э": 8, "ю": 8, "ф": 10, "щ": 10, "ъ": 10 }
+scramble = []
+word = str(input("Enter a word: "))
+scramble.append(word)
+scram = list(scramble[0])
+count = 0
+for item in scram:
+    for item1 in ar1:
+        if item == item1:
+            count += ar1[item1]
+print(count)        
+
+
+
+            
+    
 
